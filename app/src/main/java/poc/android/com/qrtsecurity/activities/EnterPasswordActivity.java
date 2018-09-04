4fb41a116da15dd60e3e0c6f71291ae5efad1ba0
Ashutosh Mishra <asheesh.gupta@krantitechservices.com>
2018-09-04 18:46:05 +0530

    Ui Changes

--- app/src/main/java/poc/android/com/qrtsecurity/activities/EnterPasswordActivity.java
+++ app/src/main/java/poc/android/com/qrtsecurity/activities/EnterPasswordActivity.java
@@ -1,7 +1,9 @@
 package poc.android.com.qrtsecurity.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import poc.android.com.qrtsecurity.R;
 
@@ -11,5 +13,19 @@ public class EnterPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_password);
+        // Method to set the UI
+        setUI();
+    }
+
+    /**
+     * Method to set the layout and UI elements
+     */
+    void setUI(){
+        findViewById(R.id.btn_forgot_password).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EnterPasswordActivity.this, CreatePasswordActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/poc/android/com/qrtsecurity/activities/SignInActivity.java
+++ app/src/main/java/poc/android/com/qrtsecurity/activities/SignInActivity.java
@@ -1,7 +1,9 @@
 package poc.android.com.qrtsecurity.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import poc.android.com.qrtsecurity.R;
 
@@ -11,5 +13,19 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        // Method to set the UI
+        setUI();
+    }
+
+    /**
+     * Method to set the layout and UI elements
+     */
+    void setUI(){
+        findViewById(R.id.btn_sign_in).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this, EnterPasswordActivity.class));
+            }
+        });
     }
 }
