b56ca00895d1a077c836f800ecfaceb0e8ec69f3
Ashutosh Mishra <asheesh.gupta@krantitechservices.com>
2018-09-04 18:09:51 +0530

    first commit

---
+++ app/src/main/java/poc/android/com/qrtsecurity/activities/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package poc.android.com.qrtsecurity.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import poc.android.com.qrtsecurity.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
