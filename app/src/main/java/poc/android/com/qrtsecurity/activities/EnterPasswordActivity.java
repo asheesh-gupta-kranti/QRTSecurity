f41c2689237d0e29914f5b923a05bf8b8e10b315
Ashutosh Mishra <asheesh.gupta@krantitechservices.com>
2018-09-04 18:32:10 +0530

    password screen

---
+++ app/src/main/java/poc/android/com/qrtsecurity/activities/EnterPasswordActivity.java
@@ -0,0 +1,15 @@
+package poc.android.com.qrtsecurity.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import poc.android.com.qrtsecurity.R;
+
+public class EnterPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_password);
+    }
+}
--- app/src/main/java/poc/android/com/qrtsecurity/activities/SplashScreenActivity.java
+++ app/src/main/java/poc/android/com/qrtsecurity/activities/SplashScreenActivity.java
@@ -1,7 +1,9 @@
 package poc.android.com.qrtsecurity.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import poc.android.com.qrtsecurity.R;
 
@@ -11,5 +13,20 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // Method to set the UI
+        setUI();
+    }
+
+    /**
+     * Method to set the layout and UI elements
+     */
+    void setUI(){
+        findViewById(R.id.btn_get_started).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+            }
+        });
     }
 }
